4aab51f9c16ab9828d2f028a39ff5ef85a833a63
Fmstrat <nospam@nowsci.com>
2021-03-31 13:34:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Pushover Desktop
+
+An electron wrapped Pushover application.
+
+## Usage
+
+Download the latest release from: https://github.com/Fmstrat/pushover-desktop/releases
+
+- After your first login, you will be taken to the Pushover console
+- Go to `Desktop` in the top nav, and register your device
+- From here out, every time you open Pushover you will be taken to the client display
+
+## Compiling yourself
+Docker is required to build images.
+
+Create the nativefier image for docker:
+``` bash
+./makeApps.sh build
+```
+
+Run the build:
+``` bash
+./makeApps.sh
+```
+
+## Ubuntu icon/shortcut
+The `shortcuts/pushover.desktop` file can be used as a shortcut from GNOME.
+
+## Windows icon fix
+
+If issues exist with the launch icon in Windows, use Rcedit from: https://github.com/electron/rcedit/releases
+```
+rcedit-x64.exe "C:\Program Files\Pushover\Pushover.exe" --set-icon resources/icon.png
+```
---
+++ makeApps.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "build" ]; then
+	cd /tmp
+	git clone https://github.com/jiahaog/nativefier.git
+	cd nativefier
+	docker build -t nativefier .
+	exit
+fi
+
+# Setup
+sudo rm -rf compile binaries
+mkdir -p compile
+mkdir -p binaries
+cp resources/* compile
+chmod a+rw compile -R
+chmod a+rw binaries -R
+cd compile
+
+# Linux
+docker run -v "${PWD}":/target nativefier --name Pushover -p linux https://client.pushover.net /target/
+sudo chown $(id -u):$(id -g) . -R
+mv Pushover-linux-x64 pushover
+tar cvfz pushover-linux.tgz pushover
+mv pushover-linux.tgz ../binaries
+
+# Windows
+docker run -v "${PWD}":/target nativefier --name Pushover -i /target/icon.png -p windows https://client.pushover.net /target/
+sudo chown $(id -u):$(id -g) . -R
+mv Pushover-win32-x64 Pushover
+zip -r Pushover-win.zip Pushover
+mv Pushover-win.zip ../binaries
+
+# OSX
+docker run -v "${PWD}":/target nativefier --name Pushover -p osx https://client.pushover.net /target/
+sudo chown $(id -u):$(id -g) . -R
+cd Pushover-darwin-x64
+zip -r Pushover-osx.app.zip Pushover.app
+mv Pushover-osx.app.zip ../../binaries
+cd ..
+
+# Cleanup
+cd ..
+sudo rm -rf compile
+sudo chown $(id -u):$(id -g) binaries -R
+
